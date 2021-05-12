b2f193ec069488e182beb3dbefae40b9ee2e49cd
Aymen <60004748+onizuka341@users.noreply.github.com>
2021-05-12 17:19:44 +0100

    fixed the op command's bug

--- commands/op.js
+++ commands/op.js
@@ -3,15 +3,21 @@ const { connect } = require("mongoose");
 const yts = require("yt-search");
 const ytdl = require("ytdl-core");
 const { Points } = require("../models");
+const { API_KEY } = require("../config.json");
 const queue = new Map();
+var stringSimilarity = require("string-similarity");
+let playing = false;
+
 module.exports.run = async (bot, message, args) => {
+  if(playing){
+    return message.reply("** I'm Afraid that you can't play until the current song ends **")
+  }
   const url = "https://api.jikan.moe/v3/top/anime/1/bypopularity";
 
   const voice_channel = message.member.voice.channel;
-  if(!voice_channel){
+  if (!voice_channel) {
     return message.reply("You need to be in a voice channel");
-
-  }  
+  }
 
   const permissions = voice_channel.permissionsFor(bot.user);
 
@@ -21,23 +27,33 @@ module.exports.run = async (bot, message, args) => {
     return message.reply("I don't have the permissions");
   axios.get(url).then(async (res) => {
     const anime = res.data.top[Math.floor(Math.random() * res.data.top.length)];
-    const video = await yts(`${anime.title} op 1`);
-    const song = { url: video.videos[0].url };
-    console.log(song.url);
-    const connection = await voice_channel.join();
-    const stream = ytdl(song.url, { filter: "audioonly" });
 
-    connection.play(stream);
-    const filter = (m) => m.member.id === message.member.id;
+    const query = anime.title + " op";
+    const connection = await voice_channel.join();
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=${API_KEY}`;
+    axios.get(url).then((res) => {
+      const data = res.data;
+      const video = data.items[0];
+      const id = video.id.videoId;
+      
+      play(id, connection)
+      playing=true
+      
+
+      
+    });
+
+    const filter = (m) => m.channel.id === message.channel.id;
     const collector = message.channel.createMessageCollector(filter, {
       time: 30000,
     });
     let answered = false;
     collector.on("collect", async (m) => {
-      if (
-        anime.title.toLowerCase().substring(0, 10) ===
-        m.content.toLowerCase().substring(0, 10)
-      ) {
+       var similarity = stringSimilarity.compareTwoStrings(m.content,anime.title );
+       if(!m.author.bot){
+         console.log(similarity)
+       }
+       if(similarity >= 0.55){
         const points = Math.floor(Math.random() * 30);
         let user = await Points.findOne({
           where: {
@@ -52,6 +68,7 @@ module.exports.run = async (bot, message, args) => {
           user.save();
         }
         answered = true;
+        playing=false;
         connection.dispatcher.end();
         message.channel.send(
           `<@${m.member.id}> **You got the answer and you earned ${points}$**`
@@ -61,11 +78,26 @@ module.exports.run = async (bot, message, args) => {
     collector.on("end", (m) => {
       if (!answered) {
         message.channel.send(`**Timeout the answer was ${anime.title}**`);
-        connection.dispatcher.end();
+        try{
+          connection.dispatcher.end();
+          playing=false
+
+        }catch{
+          playing=false
+        }
+        
       }
     });
   });
-}
+};
+const play = async (id, connection,playing) => {
+  const stream = await ytdl(id, { filter: "audioonly" });
+  connection.play(stream);
+
+  
+  
+
+};
 
 module.exports.config = {
   name: "op",
